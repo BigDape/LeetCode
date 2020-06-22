3d3bb4195dc1098464e2f9ac427c407edaec4588
zhangshuanggui <1032422400@qq.com>
2020-06-22 13:50:59 +0800

    2020,06, 22

--- zsg/a上库.bat
+++ zsg/a上库.bat
@@ -1,12 +1,8 @@
-rem start "C:\Windows\System32\cmd.exe"
 cd  /d D:
 cd D:\LeetCode\zsg
-set varyear = %date:~0,4%
-set varmonth = %date:~5,2%
-set varday = %date:~8,2%
-set var = %varyear%,%varmonth%, %varday%
+set var= %date:~0,4%,%date:~5,2%, %date:~8,2%
 
 git add .
-git commit -m "^%var^%"
+git commit -m "%var%"
 git push
 pause
